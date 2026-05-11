ffceb20e3dee715a5fd71d6cd5abe356fc4181ff
Гайсюк Вілгельм 141 (2-1) <haisiuk.vilhelm@chnu.edu.ua>
2026-05-11 17:22:45 +0300

    swagger: make a swagger for album

--- routes/albumRoutes.js
+++ routes/albumRoutes.js
@@ -2,8 +2,68 @@ const express = require('express');
 const router = express.Router();
 const albumController = require('../controllers/albumController');
 
+/**
+ * @swagger
+ * /api/albums:
+ *   get:
+ *     summary: Отримати список усіх альбомів
+ *     tags: [Albums]
+ *     responses:
+ *       200:
+ *         description: Список альбомів
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Album'
+ *   post:
+ *     summary: Створити новий альбом
+ *     tags: [Albums]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required: [title]
+ *             properties:
+ *               title:
+ *                 type: string
+ *               coverUrl:
+ *                 type: string
+ *               releaseYear:
+ *                 type: integer
+ *     responses:
+ *       201:
+ *         description: Альбом створено
+ */
 router.get('/', albumController.getAll);
-router.get('/:id', albumController.getById);
 router.post('/', albumController.create);
+
+/**
+ * @swagger
+ * /api/albums/{id}:
+ *   get:
+ *     summary: Отримати альбом за ID
+ *     tags: [Albums]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID альбому
+ *     responses:
+ *       200:
+ *         description: Дані альбому
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Album'
+ *       404:
+ *         description: Альбом не знайдено
+ */
+router.get('/:id', albumController.getById);
 
 module.exports = router;
